af7785e38bfc378000070e5810f65bdffb8a8541
Joel <valenciajoel0@gmail.com>
2023-05-08 23:03:17 -0300

    Correccion de errores

--- tienda-nube/src/main.js
+++ tienda-nube/src/main.js
@@ -2,6 +2,7 @@ import './assets/main.css'
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { createApp } from 'vue'
-import App from './components/Home.vue'
+//import App from './components/Home.vue'
+import App from './App.vue'
 
 createApp(App).mount('#app')
