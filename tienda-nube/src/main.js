a9cd3c45254f2278023e1fa66d480dc70227a8a9
Joel <valenciajoel0@gmail.com>
2023-05-08 22:48:55 -0300

    implementacion de bootstrap

--- tienda-nube/src/main.js
+++ tienda-nube/src/main.js
@@ -1,6 +1,7 @@
 import './assets/main.css'
-
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from './components/Home.vue'
 
 createApp(App).mount('#app')
