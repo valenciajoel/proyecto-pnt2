3494cbb1509c2144dca97e8af68a0dbc119df0a2
Joel <valenciajoel0@gmail.com>
2023-05-15 20:10:00 -0300

    generacion de rutas de componentes dentro de router. Uso de vue-router

--- tienda-nube/src/main.js
+++ tienda-nube/src/main.js
@@ -1,9 +1,25 @@
-import './assets/main.css'
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { createApp } from 'vue'
-import App from './components/Home.vue'
-//import App from './App.vue'
-//import App from './components/Contacto.vue'
 
-createApp(App).mount('#app')
+import * as bootstrap from 'bootstrap';
+
+import { createApp } from 'vue';
+import {useRouter,createWebHistory} from 'vue-router';
+import Produtos from './components/Productos.vue'
+import Login from './components/Login.vue'
+import Home from './App.vue'
+import Contacto from './components/Contacto.vue'
+
+const routes = [
+    { path: '/', component: Home},
+    { path: '/Login', component: Login},
+    { path: '/Contacto', component: Contacto},
+    { path: '/Producto', component: Producto},
+]
+
+const router = useRouter({
+    routes,
+    history:createWebHistory()
+})
+
+const app = createApp(Home)
+.use(router)
+.mount('#app')
