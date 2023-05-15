07170a1fd2ae9eb2b7a21f6ac3c16a16d3f56bc6
Joel <valenciajoel0@gmail.com>
2023-05-15 20:15:08 -0300

    se agrego bootstrap al proyecto

--- tienda-nube/src/main.js
+++ tienda-nube/src/main.js
@@ -1,18 +1,19 @@
 
 import * as bootstrap from 'bootstrap';
-
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
 import { createApp } from 'vue';
 import {useRouter,createWebHistory} from 'vue-router';
-import Produtos from './components/Productos.vue'
+import Productos from './components/Productos.vue'
 import Login from './components/Login.vue'
-import Home from './App.vue'
+import Home from './components/Home.vue'
 import Contacto from './components/Contacto.vue'
 
 const routes = [
     { path: '/', component: Home},
     { path: '/Login', component: Login},
     { path: '/Contacto', component: Contacto},
-    { path: '/Producto', component: Producto},
+    { path: '/Producto', component: Productos},
 ]
 
 const router = useRouter({
