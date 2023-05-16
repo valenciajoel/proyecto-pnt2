918dcda41460170e8b225cdf092132ae82f6e60b
[valen] <[vaalen.tagliaferro@hotmail.com]>
2023-05-15 22:31:15 -0300

    productos separados y aleatorio en Productos

--- tienda-nube/src/components/Productos.vue
+++ tienda-nube/src/components/Productos.vue
@@ -1,74 +1,55 @@
 <template>
     <div>
-        <h1>Imágenes de productos de ropa</h1>
-        <ProductGallery :products="products" />
+      <h1>Productos</h1>
+      <div class="image-container">
+        <div v-for="product in displayedProducts" :key="product.id" class="product-column">
+          <img :src="product.image" :alt="product.name" class="product-image" />
+          <p>{{ product.name }}</p>
+          <p>Precio: {{ product.price }}</p>
+        </div>
+      </div>
     </div>
-</template>
+  </template>
   
-<script>
-import ProductGallery from './ProductGallery.vue'; // Componente separado para la galería de productos
-
-export default {
-    components: {
-        ProductGallery,
-    },
+  <script>
+  import { products } from "@/products.js";
+  import { shuffle } from "lodash";
+  
+  export default {
     data() {
-        return {
-            products: [
-                {
-                    id: 1,
-                    name: "Buzo",
-                    image: "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/596/994/products/mdw-editado_1661-7d7cc0055546d632fa16832270719927-640-0.webp",
-                },
-                {
-                    id: 2,
-                    name: "Campera",
-                    image: "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/596/994/products/campera-puffer1-a8d41eb1ca5874077d16820084541256-640-0.webp",
-                },
-                {
-                    id: 3,
-                    name: "Pantalones",
-                    image: "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/596/994/products/carpenter1-1fab18769ad15a609716776104184092-640-0.webp",
-                },
-                {
-                    id: 4,
-                    name: "Chaleco",
-                    image: "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/596/994/products/chaleco-lacar1-ba94cb5e288e94eb7216820095270646-640-0.webp",
-                },
-                {
-                    id: 5,
-                    name: "CropTop",
-                    image: "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/596/994/products/remera-iggy1-ad05834ebb5b0aae9a16818431433671-640-0.webp",
-                },
-                {
-                    id: 6,
-                    name: "Camisa",
-                    image: "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/596/994/products/ontario1-9a3fa51412bf6c4c6516814840899168-1024-1024.webp",
-                },
-                {
-                    id: 7,
-                    name: "Buzo",
-                    image: "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/596/994/products/dsc071431-37bf49a73d1e82dd1516693326419623-1024-1024.webp",
-                },
-                {
-                    id: 8,
-                    name: "Polar",
-                    image: "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/596/994/products/mdw-color_1951-f3c31408c53ab456ad16835612021448-1024-1024.webp",
-                },
-                {
-                    id: 9,
-                    name: "Jogger",
-                    image: "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/596/994/products/mdw-color_1671-398047b178fd831dbf16835648415010-1024-1024.webp",
-                },
-                {
-                    id: 10,
-                    name: "Medias",
-                    image: "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/596/994/products/dsc001771-6a38569713256dc3fd16662036019314-1024-1024.webp",
-                },
-                // ... otros productos ...
-            ],
-        };
+      return {
+        displayedProducts: [],
+      };
     },
-};
-</script>
+    created() {
+      this.displayedProducts = shuffle(products).slice(0, 9);
+    },
+  };
+  </script>
   
+<style scoped>
+.image-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin-top: 20px;
+}
+
+.product-column {
+    width: calc(33.33% - 30px);
+    padding: 10px;
+    margin-right: 20px;
+    margin-bottom: 20px;
+    box-sizing: border-box;
+}
+
+.product-column:nth-child(3n+3) {
+    margin-right: 0;
+}
+
+.product-image {
+    width: 100%;
+    max-width: 100%;
+    height: auto;
+}</style>
+
