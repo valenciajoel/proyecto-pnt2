fe0c6cf76700f3dddc7ee1564733d991e3324fe1
Noelia Narduzzi <noelianarduzzi@gmail.com>
2023-05-21 20:11:25 -0300

    Terminando estilos en Login/Registro

--- tienda-nube/src/components/Cartel.vue
+++ tienda-nube/src/components/Cartel.vue
@@ -7,8 +7,8 @@
   </template>
   
   <style scoped>
-  .cartel {
-    position: fixed;
+  .cartelRegistro {
+    position:fixed;
     top: 0;
     left: 0;
     width: 100%;
@@ -17,6 +17,23 @@
     display: flex;
     justify-content: center;
     align-items: center;
+    overflow-y: auto;
+    padding-top: 400px;
+    
+  }
+
+  .cartelLogin {
+    position:fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    overflow-y: auto;
+    
   }
   
   .cartel-content {
@@ -24,6 +41,8 @@
     padding: 20px;
     border-radius: 5px;
     box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+
   }
+ 
   </style>
   
--- tienda-nube/src/components/Login.vue
+++ tienda-nube/src/components/Login.vue
@@ -1,108 +1,276 @@
 <template>
-    <div>
-      <button @click="mostarCartelLogin">Login</button>
-      <cartel v-if="mostarCartel" @close="cerrarCarterLogin">
-        <form>
-          <h2>Inicie sesion</h2>
-        <label for="email">Correo electrónico:</label>
-        <input type="email" id="email" required>
-        <label for="password">Contraseña:</label>
-        <input type="password" id="password" required>
-        <button type="submit">Iniciar sesión</button>
+  <div>
+    <button @click="mostrarCartelLogin">Login</button>
+    <cartel v-if="mostrarCartel" class="cartelLogin" @close="cerrarCartelLogin">
+      <form @submit.prevent="iniciarSesion">
+        <h2>Inicie sesion</h2>
+        <label for="email" class="form-label mb-3">Correo electrónico:</label>
+        <input v-model="datosInicio.email" type="email" class="form-control" id="email" required>
+
+        <label for="contraseña" class="form-label mb-3">Contraseña:</label>
+        <input v-model="datosInicio.password" type="password" class="form-control" id="contraseña" required>
+        <br>
+        <button type="button" class=" btn btn-primary form-control mb-3">Iniciar con Google</button>
+        <button type="submit" class=" btn btn-primary form-control mb-3">Iniciar sesión</button>
       </form>
-      <button @click="registrarse">Registarse</button>
-    </cartel>
-    
-    <cartel v-if="mostarCartelRegistro" @click="EnivarDatos">
-      <form>
-  <div class="form-group">
-    <label for="nombre">Nombre/s</label>
-    <input type="text" class="form-control" id="nombreEmail1" aria-describedby="nombreHelp" placeholder="Maria">
-    <small id="nombreHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="apellido">Apellido</label>
-    <input type="text" class="form-control" id="apellido" aria-describedby="apellidoHelp" placeholder="Lopez">
-    <small id="apellidoHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="email">Email</label>
-    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="ejemplo@gamil.com">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="edad">Edad</label>
-    <input type="number" class="form-control" id="edad" aria-describedby="edadlHelp" placeholder="18">
-    <small id="edadHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="dni">DNI</label>
-    <input type="number" class="form-control" id="dni" aria-describedby="dniHelp" placeholder="35648957">
-    <small id="dniHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="telefono">Telefono</label>
-    <input type="tel" class="form-control" id="telefono" aria-describedby="telefonoHelp" placeholder="31135628475">
-    <small id="telefonoHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="calle">Calle</label>
-    <input type="text" class="form-control" id="calle" aria-describedby="calleHelp" placeholder="mitre">
-    <small id="calleHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="numerocalle">Altura</label>
-    <input type="text" class="form-control" id="numerocalle" aria-describedby="numerocalHelp" placeholder="4569">
-    <small id="numerocalHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="codigoPostal">Altura</label>
-    <input type="text" class="form-control" id="codigoPostal" aria-describedby="codigoPostalHelp" placeholder="CP1043">
-    <small id="codigoPostalHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="Localidad">Localidad</label>
-    <select class="form-control" id="Localidad">
-      <option>Ciudad Autonoma de Buenos Aires</option>
-      <option>GBA-OESTA</option>
-      <option>GBA-NORTE</option>
-      <option>GBA-SUR</option>
-    </select>
-  </div>
-  <div class="form-group">
-    <label for="ciudad">Altura</label>
-    <input type="text" class="form-control" id="ciudad" aria-describedby="ciudadHelp" placeholder="Lomas de Zamora">
-    <small id="ciudadHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+      <button @click="registrarse" class=" btn btn-primary form-control mb-3">Registarse</button>
 
+      <div v-if="inicioSesionExitoso" class=" btn btn-danger">
+        Has iniciado sesión correctamente.
+      </div>
+      <div v-if="mostrarMensajeError" class=" btn btn-danger">
+        Correo electrónico o contraseña incorrectos.
+      </div>
     </cartel>
+
+
+    <div class="col-md-3">
+      <cartel v-if="mostrarCartelRegistro" class="cartelRegistro" @submit="registrarUsuario">
+        <form @submit.prevent="registrarUsuario">
+
+          <h2>Registrarse</h2>
+
+          <div class="row">
+            <div class="col-md-6">
+              <label for="nombre" class="form-label">Nombre/s:</label>
+              <input v-model="registro.nombre" type="text" class="form-control" id="nombre" required>
+            </div>
+
+            <div class="col-md-6">
+              <label for="apellido" class="form-label">Apellido:</label>
+              <input v-model="registro.apellido" type="text" class="form-control" id="apellido" required>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-6">
+              <label for="email" class="form-label">Correo electrónico:</label>
+              <input v-model="registro.email" type="email" class="form-control" id="email" required>
+            </div>
+
+            <div class="col-md-6">
+              <label for="password" class="form-label">Contraseña:</label>
+              <input v-model="registro.password" type="password" class="form-control" id="password" required>
+              <span class="text-secondary">La contraseña debe tener al menos 8 caracteres, una mayúscula, una minúscula y un número.</span>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-6">
+              <label for="edad" class="form-label">Edad:</label>
+              <input v-model="registro.edad" type="number" class="form-control" id="edad" min="18" required>
+            </div>
+
+            <div class="col-md-6">
+              <label for="dni" class="form-label">DNI:</label>
+              <input v-model="registro.dni" type="number" class="form-control" id="dni" maxlength="8" required
+                pattern="^\d{8}$">
+                <span class="text-secondary">El dni debe ser sin puntos.</span>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-6">
+              <label for="telefono" class="form-label">Teléfono:</label>
+              <input v-model="registro.telefono" type="tel" class="form-control" id="telefono" required
+                pattern="^\d{10}$">
+                <span class="text-secondary">El telefono debe ser sin 0.</span>
+            </div>
+
+            <div class="col-md-6">
+              <label for="calle" class="form-label">Calle:</label>
+              <input v-model="registro.calle" type="text" class="form-control" id="calle" required>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-6">
+              <label for="numeroCalle" class="form-label">Altura:</label>
+              <input v-model="registro.numeroCalle" type="number" class="form-control" id="numeroCalle" required>
+            </div>
+
+            <div class="col-md-6">
+              <label for="codigoPostal" class="form-label">Codigo Postal:</label>
+              <input v-model="registro.codigoPostal" type="number" class="form-control" id="codigoPostal" maxlength="4" required>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-6">
+              <label for="ciudad" class="form-label">Ciudad:</label>
+              <select v-model="registro.ciudad" class="form-control" id="ciudad" required>
+                <option value="">Selecciona una ciudad</option>
+                <option v-for="opcion in opcionesCiudad" :value="opcion.value" :key="opcion.value">{{ opcion.label }}
+                </option>
+              </select>
+            </div>
+
+            <div class="col-md-6">
+              <label for="localidad" class="form-label">Localidad:</label>
+              <input v-model="registro.localidad" type="text" class="form-control " id="localidad" required>
+            </div>
+          </div>
+          <br>
+          <div class="row">
+            <button type="submit" class="btn btn-primary form-control mb-3">Registrarse</button>
+
+            <button @click="cerrarCartelRegistro" type="submit"
+              class="btn btn-primary form-control mb-3">Cancelar</button>
+          </div>
+          <div v-if="contraseñaInvalida" class=" btn btn-danger">
+            La contraseña no es valida.
+          </div>
+          <div v-if="dniYaRegistrado" class=" btn btn-danger">
+            El DNI ya se encuentra registrado
+          </div>
+
+        </form>
+      </cartel>
     </div>
-    
-  </template>
-  
-  <script setup>
-  import { ref } from 'vue';
-  import Cartel from './Cartel.vue';
-    
-      const mostarCartel = ref(false);
-      const mostarCartelRegistro = ref(false);
-  
-      function mostarCartelLogin() {
-        mostarCartel.value = true;
-      }
-  
-      function cerrarCarterLogin() {
-        mostarCartel.value = false;
-      }
 
-      function registrarse(){
-        mostarCartelRegistro.value = true;
-      }
 
-      function EnivarDatos(){
-        //debe enviar datos
-      }
+  </div>
+</template>
   
-  </script>
+<script setup>
+import { ref } from 'vue';
+import Cartel from './Cartel.vue';
+
+
+const mostrarCartel = ref(false);
+const mostrarCartelRegistro = ref(false);
+const usuarios = ref([]);
+const inicioSesionExitoso = ref();
+const mostrarMensajeError = ref(false);
+const contraseñaInvalida = ref()
+const mostrarErrorContraseña = ref(false)
+const dniYaRegistrado = ref()
+const mostrarDniYaRegistrado = ref()
+
+const registro = ref({
+  nombre: '',
+  apellido: '',
+  email: '',
+  password: '',
+  edad: '',
+  dni: '',
+  telefono: '',
+  calle: '',
+  numeroCalle: '',
+  codigoPostal: '',
+  ciudad: '',
+  localidad: ''
+});
+
+
+
+const opcionesCiudad = [
+  { value: '1', label: 'Ciudad Autónoma de Buenos Aires' },
+  { value: '2', label: 'GBA-Oeste' },
+  { value: '3', label: 'GBA-Norte' },
+  { value: '4', label: 'GBA-Sur' },
+];
+
+const datosInicio = ref({
+  email: '',
+  password: ''
+})
+
+
+function mostrarCartelLogin() {
+  mostrarCartel.value = true;
+}
+
+function cerrarCartelLogin() {
+  mostrarCartel.value = false;
+}
+
+function registrarse() {
+  mostrarCartelRegistro.value = true;
+}
+
+function cerrarCartelRegistro() {
+  mostrarCartelRegistro.value = false;
+}
+
+const validarContrasena = (contrasena) => {
+  const longitudMinima = 8;
+  const longitudMaxima = 16;
+  const tieneMayuscula = /[A-Z]/.test(contrasena);
+  const tieneMinuscula = /[a-z]/.test(contrasena);
+  const tieneNumero = /\d/.test(contrasena);
+
+  if (
+    contrasena.length < longitudMinima ||
+    contrasena.length > longitudMaxima ||
+    !tieneMayuscula ||
+    !tieneMinuscula ||
+    !tieneNumero
+  ) {
+    return false;
+  }
+  return true;
+};
+
+
+const registrarUsuario = () => {
+
+  //Verifico que no exista el dni
+  const dniExistente = usuarios.value.some((usuario) => usuario.dni === registro.value.dni);
+
+  if (!dniExistente) {
+    if (validarContrasena(registro.value.password)) {
+      //guardo en el array al usuario 
+      usuarios.value.push(registro.value);
+    } else {
+      mostrarErrorContraseña.value = true;
+    }
+  } else {
+    mostrarDniYaRegistrado.value = true;
+  }
+
+  console.log(usuarios.value);
+
+
+  //Reincio el formulario
+  registro.value = {
+    nombre: '',
+    apellido: '',
+    email: '',
+    password: '',
+    edad: '',
+    dni: '',
+    telefono: '',
+    calle: '',
+    numeroCalle: '',
+    codigoPostal: '',
+    ciudad: '',
+    localidad: ''
+  };
+};
+
+
+
+function iniciarSesion() {
+
+  console.log(datosInicio.value.email);
+  console.log(datosInicio.value.password);
+
+  const usuarioEncontrado = usuarios.value.find(
+    (usuario) => usuario.email === datosInicio.value.email && usuario.password === datosInicio.value.password
+
+  );
+
+
+  if (usuarioEncontrado) {
+    console.log(usuarioEncontrado.email);
+    inicioSesionExitoso.value = true;
+    mostrarMensajeError.value = false;
+  } else {
+    inicioSesionExitoso.value = false;
+    mostrarMensajeError.value = true;
+  }
+}
+
+</script>
+
