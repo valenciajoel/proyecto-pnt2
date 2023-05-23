aba7d6c5d02b54ecfc42fc2beeeb60d6e375f287
Joel <valenciajoel0@gmail.com>
2023-05-22 21:58:33 -0300

    Añadi Logo

--- tienda-nube/src/App.vue
+++ tienda-nube/src/App.vue
@@ -30,55 +30,45 @@ const closeLogin = () => {
 </script>
 
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">Mi sitio web</a>
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarNav"
-      aria-controls="navbarNav"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src="./components/FLAB_Icon.jpg" alt="Logo" width="120" height="60" class="d-inline-block align-text-top">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item active">
-          <button @click="home">Home</button>
-        </li>
-        <li class="nav-item">
-          <button @click="catalogo">Catalogo</button>
-        </li>
-        <li class="nav-item">
-          <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#LoginUser" @click="openLogin">
-  Acceder
-</button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" aria-current="page" href="#" @click="home">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" @click="catalogo" href="#">Catalogo</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" @click="openLogin" href="#" data-bs-toggle="modal" data-bs-target="#LoginUser">Acceder</a>
+            <!-- Modal -->
+          <div class="modal fade" id="LoginUser" tabindex="-1" aria-labelledby="modalLogin" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <!--<h1 class="modal-title fs-5" id="modalLogin">Modal title</h1>-->
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  <Login v-if="showLoginContent" @close="closeLogin" />
+                </div>
 
-<!-- Modal -->
-<div class="modal fade" id="LoginUser" tabindex="-1" aria-labelledby="modalLogin" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="modalLogin">Modal title</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+            </div>
+          </div>
+          </li>
+        </ul>
       </div>
-      <div class="modal-body">
-        <Login v-if="showLoginContent" @close="closeLogin"/>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
-        </li>
-      </ul>
     </div>
   </nav>
+  
   <router-view></router-view>
 </template>
 
