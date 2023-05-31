5fc27e1a9794e57944e23ecfbebe20f2fe686b87
Noelia Narduzzi <noelianarduzzi@gmail.com>
2023-05-31 14:45:41 -0300

    Creando usuario administrador

--- tienda-nube/src/store.js
+++ tienda-nube/src/store.js
@@ -1,22 +1,42 @@
-import {defineStore} from 'pinia'
-import {ref, computed} from 'vue'
+import { defineStore } from 'pinia'
+import { ref, computed } from 'vue'
 
-export const useAuthStore  = defineStore('auth', () => {
+export const useAuthStore = defineStore('auth', () => {
+
+  const usuario = ref(null)
+  const usuarios = ref([]);
+  const administrador = ref(null)
+
+  function setUsuarios(usuariosRecibidos) {
+    usuarios.value = usuariosRecibidos;
     
-    const usuario = ref(null)
+  }
 
-    function iniciarSesion(usuarioRecibido) {
-          usuario.value = usuarioRecibido;
+  function iniciarSesion(datosRecibidos) {
+    
+
+    const usuarioValido = usuarios.value.find(user => user.email === datosRecibidos.value.email && user.password === datosRecibidos.value.password);
+    console.log(usuarioValido)
+
+    if (usuarioValido) {
+      if (usuarioValido.rol === 'administrador') {
+        administrador.value = usuarioValido;
+      } else {
+        usuario.value = usuarioValido;
       }
+    }
+  }
 
-      function cerrarSesion() {
-        usuario.value = null;
-      }
+  function cerrarSesion() {
+    usuario.value = null;
+  }
 
-      //es un getters
-    const hayUsuarioLogueado = computed(()=> usuario.value != null)
+  //es un getters
+  const hayUsuarioLogueado = computed(() => usuario.value != null)
+  const esAdministrador  = computed(() => administrador.value != null)
 
-      return {iniciarSesion, cerrarSesion,hayUsuarioLogueado}
+  return { iniciarSesion, cerrarSesion, hayUsuarioLogueado, setUsuarios, usuario, administrador,esAdministrador }
 
 })
-//hacer otra funcion para devolver booleano
+//hacer un usuario es administrador
+
