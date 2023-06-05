a512895eb84c552ccea36b4ac60020bccf7f9911
Noelia Narduzzi <noelianarduzzi@gmail.com>
2023-06-05 20:14:21 -0300

    Cambiando cositas en el login

--- tienda-nube/src/App.vue
+++ tienda-nube/src/App.vue
@@ -23,6 +23,10 @@ const login = () => {
   router.push("/Login");
 };
 
+const administrador = () => {
+  router.push("/Administrador");
+};
+
 import { ref } from "vue";
 import Login from "./components/Login.vue";
 
@@ -37,56 +41,43 @@ const closeLogin = () => {
 };
 
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { useAuthStore } from './store';
+
+const store = useAuthStore();
+const mostrarBotonAdministrador = ref(false);
+
+verificarAdministrador();
+
+function verificarAdministrador(){
+  if (store.esAdministrador) {
+  mostrarBotonAdministrador.value = true;
+}
+}
+
 </script>
 
 <template>
-  <link
-    rel="stylesheet"
-    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-  />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
 
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
       <a class="navbar-brand" href="#" @click="home">
-        <img
-          src="./components/FLAB_Icon.jpg"
-          alt="Logo"
-          width="40"
-          height="20"
-        />
+        <img src="./components/FLAB_Icon.jpg" alt="Logo" width="40" height="20" />
       </a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="offcanvas"
-        data-bs-target="#offcanvasNavbar"
-        aria-controls="offcanvasNavbar"
-        aria-label="Toggle navigation"
-      >
+      <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
+        aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
-      <div
-        class="offcanvas offcanvas-end"
-        tabindex="-1"
-        id="offcanvasNavbar"
-        aria-labelledby="offcanvasNavbarLabel"
-      >
+      <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
         <div class="offcanvas-header">
           <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
-          <button
-            type="button"
-            class="btn-close"
-            data-bs-dismiss="offcanvas"
-            aria-label="Close"
-          ></button>
+          <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
         <div class="offcanvas-body">
           <ul class="navbar-nav">
             <li class="nav-item">
-              <a class="nav-link" aria-current="page" href="#" @click="home"
-                >Home</a
-              >
+              <a class="nav-link" aria-current="page" href="#" @click="home">Home</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" @click="catalogo" href="#">Catalogo</a>
@@ -95,46 +86,28 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
               <a class="nav-link" @click="contacto" href="#">Contacto</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" @click="carrito" href="#"
-                >Carrito de compras</a
-              >
+              <a class="nav-link" @click="carrito" href="#">Carrito de compras</a>
             </li>
             <li class="nav-item">
-              <a
-                class="nav-link"
-                @click="openLogin"
-                href="#"
-                data-bs-toggle="modal"
-                data-bs-target="#LoginUser"
-                >Acceder</a
-              >
-              <!-- Modal -->
-              <div
-                class="modal fade"
-                id="LoginUser"
-                tabindex="-1"
-                aria-labelledby="modalLogin"
-                aria-hidden="true"
-              >
-                <div
-                  class="modal-dialog modal-dialog-centered modal-dialog-scrollable"
-                >
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <!--<h1 class="modal-title fs-5" id="modalLogin">Modal title</h1>-->
-                      <button
-                        type="button"
-                        class="btn-close"
-                        data-bs-dismiss="modal"
-                        aria-label="Close"
-                      ></button>
-                    </div>
-                    <div class="modal-body">
-                      <Login v-if="showLoginContent" @close="closeLogin" />
-                    </div>
+              <a class="nav-link" @click="openLogin" href="#" data-bs-toggle="modal"
+                data-bs-target="#LoginUser">Acceder</a>
+            <li class="nav-item">
+              <a class="nav-link" v-if="mostrarBotonAdministrador" @click="administrador" href="#">Administrador</a>
+            </li>
+            <!-- Modal -->
+            <div class="modal fade" id="LoginUser" tabindex="-1" aria-labelledby="modalLogin" aria-hidden="true">
+              <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <!--<h1 class="modal-title fs-5" id="modalLogin">Modal title</h1>-->
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div class="modal-body">
+                    <Login v-if="showLoginContent" @close="closeLogin" />
                   </div>
                 </div>
               </div>
+            </div>
             </li>
           </ul>
         </div>
