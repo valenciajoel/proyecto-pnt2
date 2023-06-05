af146ce4f4cb847c9d6625c52e80eae2110c88d9
Noelia Narduzzi <noelianarduzzi@gmail.com>
2023-06-05 14:02:50 -0300

    Agregando comentarios

--- tienda-nube/src/store.js
+++ tienda-nube/src/store.js
@@ -7,17 +7,20 @@ export const useAuthStore = defineStore('auth', () => {
   const usuarios = ref([]);
   const administrador = ref(null)
 
+  //Recibe los datos de la api de Mockachino y los setea en usuarios
   function setUsuarios(usuariosRecibidos) {
     usuarios.value = usuariosRecibidos;
     
   }
 
+  
   function iniciarSesion(datosRecibidos) {
     
-
+    //Busca lal usuario en el array de usuario con los datos recibidos en el login
     const usuarioValido = usuarios.value.find(user => user.email === datosRecibidos.value.email && user.password === datosRecibidos.value.password);
     console.log(usuarioValido)
 
+    //Asigna al tipo usuario correspondiente
     if (usuarioValido) {
       if (usuarioValido.rol === 'administrador') {
         administrador.value = usuarioValido;
@@ -30,16 +33,17 @@ export const useAuthStore = defineStore('auth', () => {
   function cerrarSesion() {
     usuario.value = null;
   }
+
   function getUsuario(){
-    
     return usuario.value
   }
-  //es un getters
+
+  //Propiedad que devulve un booleano de si esta o no logueado.
   const hayUsuarioLogueado = computed(() => usuario.value != null)
   const esAdministrador  = computed(() => administrador.value != null)
   
   return { iniciarSesion, cerrarSesion, hayUsuarioLogueado, setUsuarios,esAdministrador,usuario }
 
 })
-//hacer un usuario es administrador
+
 
