11e51d8d7d98cc650e7e81f89c95f0a5896a264a
Noelia Narduzzi <noelianarduzzi@gmail.com>
2023-06-04 22:09:09 -0300

    modificando contacto

--- tienda-nube/src/components/Contacto.vue
+++ tienda-nube/src/components/Contacto.vue
@@ -1,42 +1,43 @@
 <template>
+  <div class="container">
     <h1>Contáctanos</h1>
     <form @submit.prevent="guardarMensaje">
-  <div class="form-group">
-    <label for="nombre">Nombre</label>
-    <input v-model="datosRecibidos.nombre" type="text" class="form-control mb-3" id="nombre" aria-describedby="nombreHelp" placeholder="Ingrese su nombre" required>
+      <div class="form-group">
+        <label for="nombre">Nombre</label>
+        <input v-model="datosRecibidos.nombre" type="text" class="form-control mb-3 col-md-6" id="nombre" aria-describedby="nombreHelp" placeholder="Ingrese su nombre" required>
+      </div>
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input v-model="datosRecibidos.email" type="email" class="form-control mb-3 col-md-6" id="email" aria-describedby="emailHelp" placeholder="Ingrese su email" required>
+      </div>
+      <div class="form-group">
+        <label for="telefono">Telefono</label>
+        <input v-model="datosRecibidos.telefono" type="tel" class="form-control mb-3 col-md-6" id="telefono" aria-describedby="telefonoHelp" placeholder="Ingrese su telefono" required>
+      </div>
+      <div class="form-group">
+        <label for="comentarios">Comentarios</label>
+        <textarea v-model="datosRecibidos.comentarios" class="form-control mb-3 col-md-6" id="comentarios" rows="3" required></textarea>
+      </div>
+      <div class="text-end">
+        <button type="submit" class="btn btn-primary mb-3">Enviar</button>
+      </div>
+    </form>
   </div>
-  <div class="form-group">
-    <label for="email">Email</label>
-    <input v-model="datosRecibidos.email" type="email" class="form-control mb-3" id="email" aria-describedby="emailHelp" placeholder="Ingrese su email" required>
-    
-  </div>
-  <div class="form-group">
-    <label for="telefono">Telefono</label>
-    <input v-model="datosRecibidos.telefono" type="tel" class="form-control mb-3" id="telefono" aria-describedby="telefonoHelp" placeholder="Ingrese su telefono" required>
-    
-  </div>
-  <div class="form-group">
-    <label for="comentarios">Comentarios</label>
-    <textarea v-model="datosRecibidos.comentarios" class="form-control mb-3" id="comentarios" rows="3" required></textarea>
-  </div>
-  <button type="submit" class="btn btn-primary mb-3">Submit</button>
-</form>
-  </template>
-  
-  <script setup>
-  
-  const mensajesRecibidos = ({})
-  import { ref } from 'vue';
+</template>
 
-  const datosRecibidos = ref({
+<script setup>
+import { ref } from 'vue';
+
+const mensajesRecibidos = ref([]);
+
+const datosRecibidos = ref({
   nombre: '',
   email: '',
   telefono: '',
   comentarios: ''
 });
 
-  function guardarMensaje(){
-    mensajesRecibidos.value.push(datosRecibidos.value);
-  }
-
-  </script>
+function guardarMensaje() {
+  mensajesRecibidos.value.push(datosRecibidos.value);
+}
+</script>
