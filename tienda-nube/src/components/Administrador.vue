c6de65aff2ea7c68ac3f8715f2c7c1040b292cdd
Noelia Narduzzi <noelianarduzzi@gmail.com>
2023-06-12 22:41:32 -0300

    Sumando graficos

--- tienda-nube/src/components/Administrador.vue
+++ tienda-nube/src/components/Administrador.vue
@@ -1,69 +1,180 @@
 <template>
-    <Bar :data="data"   />
-  </template>
+  <div>
+    <div>
+    <Bar :data="data" />
+  </div>
+
+  <div>
+    <Bar :data="dataMenosVendidos" />
+  </div>
+
+  <div>
+    <Bar :data="dataMesMenosVentas" />
+  </div>
+
+  <div>
+    <Bar :data="dataMesMasVentas" />
+  </div>
+  </div>
   
-  <script setup>
-  import {
-    Chart as ChartJS,
-    Title,
-    Tooltip,
-    Legend,
-    BarElement,
-    CategoryScale,
-    LinearScale
-  } from 'chart.js'
-  import { Bar } from 'vue-chartjs'
-  import {products} from '../products'
-  import{onMounted, ref} from "vue";
-  import { GoogleSheets } from '../connectionWithGoogle';
 
-  ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
+</template>
+  
+<script setup>
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+} from 'chart.js'
+import { Bar } from 'vue-chartjs'
+import { products } from '../products'
+import { onMounted, ref } from "vue";
+import { GoogleSheets } from '../connectionWithGoogle';
 
-  const date = {date:{month:5,year:2023}}
-  const data = ref({labels:["","",""],datasets:[{data:[0,0,0]}]})
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
 
-  onMounted(async()=>{
 
-    let aux = await  GoogleSheets.obtenerTopArticulos(date)
-    console.log(aux);
+const data = ref({ labels: ["", "", ""], datasets: [{ data: [0, 0, 0] }] })
+const date = { date: { month: 5, year: 2023 } }
+const dataMenosVendidos = ref({ labels: ["", "", ""], datasets: [{ data: [0, 0, 0] }] })
+const dataMesMenosVentas = ref({ labels: ["", "", ""], datasets: [{ data: [0, 0, 0] }] })
+const dataMesMasVentas = ref({ labels: ["", "", ""], datasets: [{ data: [0, 0, 0] }] })
 
-    const articulos = aux.response.map(item => {
-  const producto = products.find(producto => producto.id === item.art);
-  return producto ? producto.name : null;
-});
-console.log(articulos);
+onMounted(async () => {
 
-    // Obtener cantidades por separado
-    const cantidades = aux.response.map(item => item.cant);
-    console.log(cantidades);
-    
-    data.value = {labels:articulos,datasets:[{data:cantidades,backgroundColor: [
-      'rgba(255, 99, 132, 0.2)',
-      'rgba(255, 159, 64, 0.2)',
-      'rgba(255, 205, 86, 0.2)',
-      'rgba(75, 192, 192, 0.2)',
-      'rgba(54, 162, 235, 0.2)',
-      'rgba(153, 102, 255, 0.2)',
-      'rgba(201, 203, 207, 0.2)'
-    ],
-    borderColor: [
-      'rgb(255, 99, 132)',
-      'rgb(255, 159, 64)',
-      'rgb(255, 205, 86)',
-      'rgb(75, 192, 192)',
-      'rgb(54, 162, 235)',
-      'rgb(153, 102, 255)',
-      'rgb(201, 203, 207)'
-    ], borderWidth: 1}],backgroundColor: 'rgba(75, 192, 192, 0.6)', 
-      borderColor: 'rgba(75, 192, 192, 1)', // Color del borde de las barras
-      borderWidth: 1, options: {
+  let aux = await GoogleSheets.obtenerTopArticulos(date)
+
+  //trae los nombres de los productos
+  const articulos = aux.response.map(item => {
+    const producto = products.find(producto => producto.id === item.art);
+    return producto ? producto.name : null;
+  });
+
+  // Obtener cantidades por separado
+  const cantidades = aux.response.map(item => item.cant);
+
+  data.value = {
+    labels: articulos, datasets: [{
+      data: cantidades, backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 205, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(201, 203, 207, 0.2)'
+      ],
+      borderColor: [
+        'rgb(255, 99, 132)',
+        'rgb(255, 159, 64)',
+        'rgb(255, 205, 86)',
+        'rgb(75, 192, 192)',
+        'rgb(54, 162, 235)',
+        'rgb(153, 102, 255)',
+        'rgb(201, 203, 207)'
+      ], borderWidth: 1, barThickness: 250,
+    }], options: {
       scales: {
         y: {
           beginAtZero: true
         }
       }
-    },}
-  })
+    },
+  }
+
+  dataMenosVendidos.value = {
+    labels: articulos, datasets: [{
+      data: cantidades, backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 205, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(201, 203, 207, 0.2)'
+      ],
+      borderColor: [
+        'rgb(255, 99, 132)',
+        'rgb(255, 159, 64)',
+        'rgb(255, 205, 86)',
+        'rgb(75, 192, 192)',
+        'rgb(54, 162, 235)',
+        'rgb(153, 102, 255)',
+        'rgb(201, 203, 207)'
+      ], borderWidth: 1, barThickness: 250,
+    }], options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    },
+  }
+
+  dataMesMenosVentas.value = {
+    labels: articulos, datasets: [{
+      data: cantidades, backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 205, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(201, 203, 207, 0.2)'
+      ],
+      borderColor: [
+        'rgb(255, 99, 132)',
+        'rgb(255, 159, 64)',
+        'rgb(255, 205, 86)',
+        'rgb(75, 192, 192)',
+        'rgb(54, 162, 235)',
+        'rgb(153, 102, 255)',
+        'rgb(201, 203, 207)'
+      ], borderWidth: 1, barThickness: 250,
+    }], options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    },
+  }
+
+  dataMesMasVentas.value = {
+    labels: articulos, datasets: [{
+      data: cantidades, backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 205, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(201, 203, 207, 0.2)'
+      ],
+      borderColor: [
+        'rgb(255, 99, 132)',
+        'rgb(255, 159, 64)',
+        'rgb(255, 205, 86)',
+        'rgb(75, 192, 192)',
+        'rgb(54, 162, 235)',
+        'rgb(153, 102, 255)',
+        'rgb(201, 203, 207)'
+      ], borderWidth: 1, barThickness: 150,
+    }], options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    },
+  }
 
 
-  </script>
+})
+
+
+</script>
