00bb0a1efdbb624c762a504174f5b3f18a2ebdd1
valentaglia <vaalen.tagliaferro@hotmail.com>
2023-06-25 17:04:20 -0300

    Aparicion 4 graficos

--- tienda-nube/src/components/Administrador.vue
+++ tienda-nube/src/components/Administrador.vue
@@ -1,5 +1,10 @@
 <template>
-  <Bar :data="data" />
+  <div>
+    <Bar :data="data" :options="optionsPrincipal" />
+    <Bar :data="dataMenosVendidos" :options="optionsMenosVendidos" />
+    <Bar :data="dataMesMenosVentas" :options="optionsMesMenosVentas" />
+    <Bar :data="dataMesMasVentas" :options="optionsMesMasVentas" />
+  </div>
 </template>
   
   <script setup>
@@ -28,6 +33,66 @@ ChartJS.register(
 
 const date = { date: { month: 5, year: 2023 } };
 const data = ref({ labels: ["", "", ""], datasets: [{ data: [0, 0, 0] }] });
+const dataMenosVendidos = ref({
+  labels: ["", "", ""],
+  datasets: [{ data: [0, 0, 0] }],
+});
+
+const dataMesMenosVentas = ref({
+  labels: ["", "", ""],
+  datasets: [{ data: [0, 0, 0] }],
+});
+
+const dataMesMasVentas = ref({
+  labels: ["", "", ""],
+  datasets: [{ data: [0, 0, 0] }],
+});
+const optionsPrincipal = ref({
+  plugins: {
+    title: {
+      display: true,
+      text: "Artículos más vendidos",
+      fontSize: 18,
+      fontColor: "#000",
+      fontStyle: "bold",
+    },
+  },
+});
+
+const optionsMenosVendidos = ref({
+  plugins: {
+    title: {
+      display: true,
+      text: "Artículos menos vendidos",
+      fontSize: 18,
+      fontColor: "#000",
+      fontStyle: "bold",
+    },
+  },
+});
+
+const optionsMesMenosVentas = ref({
+  plugins: {
+    title: {
+      display: true,
+      text: "Mes con menos ventas",
+      fontSize: 18,
+      fontColor: "#000",
+      fontStyle: "bold",
+    },
+  },
+});
+const optionsMesMasVentas = ref({
+  plugins: {
+    title: {
+      display: true,
+      text: "Mes con más ventas",
+      fontSize: 18,
+      fontColor: "#000",
+      fontStyle: "bold",
+    },
+  },
+});
 
 onMounted(async () => {
   let aux = await GoogleSheets.obtenerTopArticulos(date);
@@ -44,26 +109,34 @@ onMounted(async () => {
   console.log(cantidades);
 
   data.value = {
-    labels: articulos, datasets: [{
-      data: cantidades, backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
-        'rgba(255, 205, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(201, 203, 207, 0.2)'
-      ],
-      borderColor: [
-        'rgb(255, 99, 132)',
-        'rgb(255, 159, 64)',
-        'rgb(255, 205, 86)',
-        'rgb(75, 192, 192)',
-        'rgb(54, 162, 235)',
-        'rgb(153, 102, 255)',
-        'rgb(201, 203, 207)'
-      ], borderWidth: 1, barThickness: 20,pointRadius: 3,
-    }], options: {
+    labels: articulos,
+    datasets: [
+      {
+        data: cantidades,
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+          "rgba(255, 205, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(201, 203, 207, 0.2)",
+        ],
+        borderColor: [
+          "rgb(255, 99, 132)",
+          "rgb(255, 159, 64)",
+          "rgb(255, 205, 86)",
+          "rgb(75, 192, 192)",
+          "rgb(54, 162, 235)",
+          "rgb(153, 102, 255)",
+          "rgb(201, 203, 207)",
+        ],
+        borderWidth: 1,
+        barThickness: 20,
+        pointRadius: 3,
+      },
+    ],
+    options: {
       scales: {
         y: {
           beginAtZero: true,
@@ -71,28 +144,34 @@ onMounted(async () => {
       },
     },
   };
-
   dataMenosVendidos.value = {
-    labels: articulosMenosVendidos, datasets: [{
-      data: cantidadesMenosVendidos,  backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
-        'rgba(255, 205, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(201, 203, 207, 0.2)'
-      ],
-      borderColor: [
-        'rgb(255, 99, 132)',
-        'rgb(255, 159, 64)',
-        'rgb(255, 205, 86)',
-        'rgb(75, 192, 192)',
-        'rgb(54, 162, 235)',
-        'rgb(153, 102, 255)',
-        'rgb(201, 203, 207)'
-      ], borderWidth: 1, barThickness: 20,
-    }], options: {
+    labels: articulosMenosVendidos,
+    datasets: [
+      {
+        data: cantidadesMenosVendidos,
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+          "rgba(255, 205, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(201, 203, 207, 0.2)",
+        ],
+        borderColor: [
+          "rgb(255, 99, 132)",
+          "rgb(255, 159, 64)",
+          "rgb(255, 205, 86)",
+          "rgb(75, 192, 192)",
+          "rgb(54, 162, 235)",
+          "rgb(153, 102, 255)",
+          "rgb(201, 203, 207)",
+        ],
+        borderWidth: 1,
+        barThickness: 20,
+      },
+    ],
+    options: {
       scales: {
         y: {
           beginAtZero: true,
